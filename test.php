aa5af68d3f4ec9ac191e0c9c3c1cc5d31b4a28e2
Christopher Fuentes <fuenteschris75@gmail.com>
2019-03-28 08:14:31 -0600

    Código para recibir código escrito por usuario y validar.

--- test.php
+++ test.php
@@ -1,11 +1,5 @@
 <?php
 
-//$codigo= $_POST["code"]; //Recibimos el código escrito por el usuario
-//$nombre= "codigo1.php"; //Generamos un nombre para el archivo
-//$archivo= fopen($nombre,"w");
-//fwrite($archivo, $codigo);
-//fclose($archivo);
-//include($nombre);
 eval(str_replace(["<?php","?>"],"",$_POST["code"]));
 echo suma(1,2);
 
